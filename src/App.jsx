26c81f00d89caed5fcdfd2b03141e1b5064e182b
Sherjan48 <kidaijalobu@mail.ru>
2025-05-22 20:41:17 +0600

    footer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './component/footer/Footer'
+import './App.css'
+import Show from './component/show/Show'
+
+function App() {
+  return (
+    <div>
+      <Show/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/show/Show.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './show.css'
+
+function Show() {
+  return (
+    <div className='show'>
+      <div className="map">
+        <h1>Контакты</h1>
+        <div className="adr"></div>
+      </div>
+      <div className="facts"></div>
+    </div>
+  )
+}
+
+export default Show
