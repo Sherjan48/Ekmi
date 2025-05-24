9c8fe875a2c181a8bb7baddb75f786bcd4ff1089
Sherjan48 <kidaijalobu@mail.ru>
2025-05-24 22:12:34 +0600

    footer

--- src/component/show/Show.jsx
+++ src/component/show/Show.jsx
@@ -1,16 +1,489 @@
-import React from 'react'
-import './show.css'
+import React from "react";
+import "./show.css";
 
 function Show() {
   return (
-    <div className='show'>
+    <div className="show">
       <div className="map">
         <h1>Контакты</h1>
         <div className="adr"></div>
       </div>
-      <div className="facts"></div>
+      <div className="facts">
+        <h1>Факты:</h1>
+        <div className="fact">
+          <div className="fact1">
+            <span> 1</span>
+            <p>
+              Осуществляемые компанией «Экми-Мебель» постоянные инвестиции в
+              сертификацию своей продукции, убедительно подтверждают лидерство
+              компании на украинском рынке высококачественной мягкой мебели.
+            </p>
+          </div>
+          <div className="fact2">
+          <span > 2</span>
+
+            <p>
+              Фабрика «Экми-мебель» сертифицирована по системе менеджмента
+              качества ISO 9001:2000 на соответствие международным стандартам,
+              представительством «Бюро Веритас» в Украине.
+            </p>
+          </div>
+          <div className="fact3">
+          <span> 3</span>
+
+            <p>
+              Компания «Экми-Мебель» первой из украинских производителей мягкой
+              мебели обеспечила свою мебель полноценной гарантией и первой
+              обоснованно заявила о лучшем качестве производимой мебели.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="fil">
+        <div className="page1">
+          <div className="infa">
+            <h1>с. Петропавловская Борщаговка</h1>
+            <div className="info">
+              <h3>Киевская Обл.</h3>
+              <h3>Адрес: Ул. Большая Окружная 4</h3>
+              <ul>
+                <li>
+                  <span>тел.: +38 (044) 502-99-70</span>
+                </li>
+                <li>
+                  <span>info@ekmi-mebli.com</span>
+                </li>
+              </ul>
+              <h3>
+                Отдел по работе с корпаративными клиентами, дилерами и экспортом
+              </h3>
+              <ul>
+                <li>
+                  <span>+3 (050) 353-36-68</span>
+                </li>
+                <li>
+                  <span>sergey_p@ekmi-mebli.com</span>
+                </li>
+              </ul>
+              <h3>Отдел по работе с дизайнерами и архитекторами</h3>
+              <ul>
+                <li>
+                  <span>тел.: +38 (067) 956-58-07</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="page2">
+          <div className="infa">
+            <h1>с. Петропавловская Борщаговка</h1>
+            <div className="info">
+              <h3>Киевская Обл.</h3>
+              <h3>Адрес: ул. Петропавловская 6</h3>
+              <h3>
+                Фирменный салон`Экми-мебель`<br></br>ТЦ «4ROOM» 2 этаж, секция
+                №338
+              </h3>
+              <ul>
+                <li>
+                  <span>Email: ekmi.araks@gmail.com</span>
+                </li>
+                <li>
+                  <span>Тел: 067-548-32-70</span>
+                </li>
+                <li>
+                  <span>Тел: 067-956-58-07 (viber)</span>
+                </li>
+              </ul>
+              <h3>Режим работы:</h3>
+              <ol>
+                <li>Режим работы: Понедельник-Воскресенье: 10:00-20:00</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+        <div className="page3">
+          <div className="inf1">
+            <h1>Винница</h1>
+            <h3>
+              Адрес: <span>ул. Келецкая 117 Б</span>
+            </h3>
+            <h3>ТЦ «ПЛАЗА Парк»</h3>
+            <h3>
+              Почта: <span>makoviy_marina@mail.ru</span>
+            </h3>
+            <h3>
+              Телефон: <span>+38 067 432 14 22</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page4">
+          <div className="inf1">
+            <h1>Винница</h1>
+            <h3>
+              Адрес: <span>ул. Зодчих 5</span>
+            </h3>
+            <h3>Мебельный салон «МІА»</h3>
+            <h3>
+              Почта: <span>makoviy_marina@mail.ru</span>
+            </h3>
+            <h3>
+              Телефон: <span>+38 067 432 14 22</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page5">
+          <div className="inf1">
+            <h1>Винница</h1>
+            <h3>
+              Адрес: <span>ул. Келецкая 81</span>
+            </h3>
+            <h3>Салон мебели «ЗЕФИР»</h3>
+            <h3>
+              Почта: <span>zefir.vip@gmail.com</span>
+            </h3>
+            <h3>
+              Телефон: <span>+38 067 432 14 22</span>
+            </h3>
+            <span className="span">+38 (0432) 504-234</span>
+          </div>
+        </div>
+        <div className="page6">
+          <div className="inf2">
+            <h1>Вознесенск</h1>
+            <h3>
+              Адрес: <span>ул. Синякова 6 — 230</span>
+            </h3>
+            <h3>Салон «Будiнок меблiв»</h3>
+            <h3>
+              Почта:<span> dom_mebeli_07@mail.ru</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 066 494 70 00</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page7">
+          <div className="inf2">
+            <h1>Ивано-Франковск</h1>
+            <h3>
+              Адрес: <span>ул. Статуса 30 Д</span>
+            </h3>
+            <h3>Интерьер-магазин «ВИЖЕН»</h3>
+            <h3>
+              Почта:<span> vizhen@ukr.net</span>
+            </h3>
+            <h3>
+              Сайт:<span> www.vizhen.com.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 066 494 70 00</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page8">
+          <div className="inf2">
+            <h1>Краматорск</h1>
+            <h3>
+              Адрес: <span> ул. Машиностроителей, 12</span>
+            </h3>
+            <h3>Мебельный центр «Флеш»</h3>
+            <h3>
+              Почта:<span> grebenyuk_vi@ukr.net</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 066 494 70 00</span>
+            </h3>
+            <span className="span">+38 (067) 612 03 21</span>
+          </div>
+        </div>
+        <div className="page9">
+          <div className="inf3">
+            <h1>Кременчуг</h1>
+            <h3>
+              Адрес: <span> ул. Первомайская 57 Б</span>
+            </h3>
+            <h3>Мебельный салон «ЭЛЕГАНС»</h3>
+            <h3>
+              Почта:<span> mebelelegance@gmail.com</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 053 674 03 12</span>
+            </h3>
+            <span className="span"> +38 097 77 37 917</span>
+          </div>
+        </div>
+        <div className="page10">
+          <div className="inf3">
+            <h1>Луцк</h1>
+            <h3>
+              Адрес: <span> ул. Ершова 11а</span>
+            </h3>
+            <h3>Мебельный салон «АВИВА»</h3>
+            <h3>
+              Почта:<span> avivalutsk@gmail.com</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 050 969 77 </span>
+            </h3>
+            <span className="span"> +38 050 152 38</span>
+          </div>
+        </div>
+        <div className="page11">
+          <div className="inf3">
+            <h1>Львов</h1>
+            <h3>
+              Адрес: <span> ул. Княгини Ольги, 95</span>
+            </h3>
+            <h3>ТЦ «MARK» 1 этаж, секция №113.</h3>
+            <h3>
+              Почта:<span> ekmilviv@ukr.net</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 097 093 96 79</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page12">
+          <div className="inf3">
+            <h1>Николаев</h1>
+            <h3>
+              Адрес: <span> ул.Василия Стуса, 4</span>
+            </h3>
+            <h3>Салон мебели «ДИЗАЙН»</h3>
+            <h3>
+              Почта:<span> tanya@dizayn.vn.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 067 430 80 47</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page13">
+          <div className="inf3">
+            <h1>Мариуполь</h1>
+            <h3>
+              Адрес: <span> ул. Харлампиевская, 33</span>
+            </h3>
+            <h3>Салон мебели «ЭКМИ-мебель»</h3>
+            <h3>
+              Почта:<span> ekmimariupol2017@gmail.com</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 (062) 949 29 65</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page14">
+          <div className="inf3">
+            <h1 className="mog">Могилев-Подольский</h1>
+            <h3>
+              Адрес: <span> ул.Василия Стуса, 4</span>
+            </h3>
+            <h3>Салон мебели «ДИЗАЙН»</h3>
+            <h3>
+              Почта:<span> tanya@dizayn.vn.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 067 430 80 47</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page15">
+          <div className="inf3">
+            <h1>Одесса</h1>
+            <h3>
+              Адрес: <span> Овидиопольская дорога 7</span>
+            </h3>
+            <h3>ТЦ 6 элемент, Галерея мебели APRIORI</h3>
+            <h3>
+              Телефон:<span> +38 (048)-701-96-95</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page16">
+          <div className="inf3">
+            <h1 className="mog">Одесса</h1>
+            <h3>
+              Адрес: <span> ул. Толбухина, 135</span>
+            </h3>
+            <h3>ТЦ Мегадом, салон CAVIO</h3>
+            <h3>
+              Почта:<span> tanya@dizayn.vn.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 (048)-736-29-25</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page17">
+          <div className="inf3">
+            <h1>Полтава</h1>
+            <h3>
+              Адрес: <span> ул. Европейськая, 107 </span>
+            </h3>
+            <h3>Салон мебели «РОДА»</h3>
+            <h3>
+              Почта:<span> igorsveta2005@rambler.ru</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 (053)-59-95-40</span>
+            </h3>
+            <span className="span"> +38 (050) 050 152 38</span>
+          </div>
+        </div>
+        <div className="page18">
+          <div className="inf4">
+            <h1>Ровно</h1>
+            <h3>
+              Адрес: <span> ул. Гагарина 18 Б</span>
+            </h3>
+            <h3>Центр мебели и декора ИНТЕРИО</h3>
+            <h3>
+              Почта:<span> interioitaly@gmail.com</span>
+            </h3>
+            <h3>
+              Сайт:<span> www.interio.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 (053)-59-95-40</span>
+            </h3>
+            <span className="span"> +38 (050) 050 152 38</span>
+          </div>
+        </div>
+        <div className="page19">
+          <div className="inf4">
+            <h1>Ужгород</h1>
+            <h3>
+              Адрес: <span> ул. Минайская, 18 г.</span>
+            </h3>
+            <h3>Салон мебели «ШЕРАТОН»</h3>
+            <h3>
+              Почта:<span> sheraton.mebel@gmail.com</span>
+            </h3>
+            <h3>
+              Сайт:<span> www.sheraton.uz</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 050 523 22 32</span>
+            </h3>
+            <span className="span"> +38 067 198 64 69</span>
+          </div>
+        </div>
+        <div className="page20">
+          <div className="inf3">
+            <h1>Сумы</h1>
+            <h3>
+              Адрес: <span> ул. Горького, 25</span>
+            </h3>
+            <h3>Мебельный салон «Империя мебели»</h3>
+            <h3>
+              Почта:<span> www.imperia-mebeli.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 0542 66 05 03</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page21">
+          <div className="inf3">
+            <h1>Тернополь</h1>
+            <h3>
+              Адрес: <span> ул.Збаразская, 18</span>
+            </h3>
+            <h3>ТЦ «АРС»</h3>
+            <h3>
+              Почта:<span> Govda@ars.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +380 352 430 000</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page22">
+          <div className="inf3">
+            <h1>Харьков</h1>
+            <h3>
+              Адрес: <span> пр. Московский 199-б,(2этаж)</span>
+            </h3>
+            <h3>ТЦ «Сан Сити премиум»</h3>
+            <h3>
+              Почта:<span> viktorykazb@gmail.com</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 097-707-41-74</span>
+            </h3>
+            <span className="span"> +38 050-965-28-70</span>
+          </div>
+        </div>
+        <div className="page23">
+          <div className="inf3">
+            <h1>Херсон</h1>
+            <h3>
+              Адрес: <span> пр.  200 лет Херсона, 2</span>
+            </h3>
+            <h3>Мебельный арт центр Nicolas</h3>
+            <h3>
+              Почта:<span> cc@nicolas.com.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 (095)-277-25-66</span>
+            </h3>
+            <span className="span"> +38 (055)-241-43-75</span>
+          </div>
+        </div>
+        <div className="page24">
+          <div className="inf4">
+            <h1>Черновцы</h1>
+            <h3>
+              Адрес: <span> ул. Гагарина, 15</span>
+            </h3>
+            <h3>Салон мебели «КОНТИНЕНТАЛЬ»</h3>
+            <h3>
+              Почта:<span>kontinental.mebel@mail.com</span>
+            </h3>
+            <h3>
+              Сайт:<span> www.kontinental.com.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 050 134 22 19</span>
+            </h3>
+            <span className="span"> +38 098 134 22 19</span>
+          </div>
+        </div>
+        <div className="page25">
+          <div className="inf3">
+            <h1>Хмельницкий</h1>
+            <h3>
+              Адрес: <span> ул. Старомиська, 52</span>
+            </h3>
+            <h3>Салон мебели «ЭКМИ-мебель»</h3>
+            <h3>
+              Почта:<span> gonchar@pr.km.ua</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 038 270 13 59</span>
+            </h3>
+          </div>
+        </div>
+        <div className="page26">
+        <div className="inf3">
+            <h1>Днепр</h1>
+            <h3>
+              Адрес: <span> пр. Александра Поля 137</span>
+            </h3>
+            <h3>Салон мебели » Gorenje»</h3>
+            <h3>
+              Почта:<span> gorenje-c-dnepr@ukr.net</span>
+            </h3>
+            <h3>
+              Телефон:<span> +38 (050)-481-78-77</span>
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Show
+export default Show;
