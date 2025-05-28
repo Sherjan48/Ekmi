8a0841788cc5be584fa61c3d618b51319b85551e
Sherjan48 <kidaijalobu@mail.ru>
2025-05-28 22:02:58 +0600

    pochinil

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,23 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./component/Layout";
 import Show from "./component/show/Show";
 import Home from "./component/home/Home";
+import Layout from "./component/Layout/Layout";
+import Model from "./component/2dmodel/Model";
+import Dmodel from "./component/3dmodel/Dmodel";
 export const myRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
+    element: <Layout />,
     children: [
       {
         path: "/",
-        element: <Home/>,
+        element: <Home />,
       },
-    //   {
-    //     path: "contactPage",
-    //     element: <Contact />,
-    //   },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "modeli",
+        element: <Model />,
+      },
+      {
+        path: "/2d-3d",
+        element: <Dmodel />,
+      },
       {
         path: "show",
-        element: <Show/>,
+        element: <Show />,
       },
     ],
   },
