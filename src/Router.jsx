cb2cd1d3a9be0168f18f6557b3d1d3b09e61ab50
Sherjan48 <kidaijalobu@mail.ru>
2025-05-30 17:47:28 +0600

    nichego

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,7 +11,7 @@ export const myRouter = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <Show />,
       },
       {
         path: "home",
